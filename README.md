a6064397f470cf6e5380f62d671f5452b56d3d5e
Ozgur Gunes <o.gunes@gmail.com>
2023-01-30 16:44:05 +0300

    Prettier

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 <img src="media/cover.png" alt="Sketch Case Converter" width="100%" />
 
-# Case Converter
+# Figma Case Converter
 
 Changes the case of text in selected nodes to `Sentence case`, `Title Case`, `UPPER CASE` or `lower case`. 
 
 Supports locale conversion, which means, it converts `istanbul` to `İSTANBUL`, if `Turkish` language chosen.
+
+
+### How to Use
+
+- Select nodes containing texts to convert.
+- Choose a conversion command under `Plugins -> Case Converter` menu or start typing the command name in Figma's quick actions search bar (`⌘` + `/` on Mac or `Ctrl` + `/` on Windows).
+- Enter the the text language by selecting one of the options (You can filter the languages quickly by typing first letters).
+- Hit `Enter` to run the plugin.
--- main.ts
+++ main.ts
@@ -1,107 +1,107 @@
-import langmap from "langmap";
-import latinize from "latinize";
-import { traverseNode } from "@create-figma-plugin/utilities";
+import langmap from 'langmap'
+import latinize from 'latinize'
+import { traverseNode } from '@create-figma-plugin/utilities'
 
 export function titleCase() {
-  convertFunction(titleCaseFunction);
+  convertFunction(titleCaseFunction)
 }
 
 export function sentenceCase() {
-  convertFunction(sentenceCaseFunction);
+  convertFunction(sentenceCaseFunction)
 }
 
 export function upperCase() {
-  convertFunction(upperCaseFunction);
+  convertFunction(upperCaseFunction)
 }
 
 export function lowerCase() {
-  convertFunction(lowerCaseFunction);
+  convertFunction(lowerCaseFunction)
 }
 
 function convertFunction(caseFunction: Function) {
   figma.parameters.on(
-    "input",
-    ({ parameters, key, query, result }: ParameterInputEvent) => {
-      const languages = getLanguages();
+    'input',
+    ({ query, result }: ParameterInputEvent) => {
+      const languages = getLanguages()
       result.setSuggestions(
-        languages.filter((s) =>
+        languages.filter(s =>
           latinize(s.name.toLowerCase()).startsWith(
             latinize(query.toLowerCase())
           )
         )
-      );
+      )
     }
-  );
-  figma.on("run", ({ command, parameters }: RunEvent) => {
-    if (typeof parameters === "undefined") {
-      throw new Error("`runEvent.parameters` is `undefined`");
+  )
+  figma.on('run', ({ parameters }: RunEvent) => {
+    if (typeof parameters === 'undefined') {
+      throw new Error('`runEvent.parameters` is `undefined`')
     }
-    convertCase(caseFunction, parameters.locale);
-  });
+    convertCase(caseFunction, parameters.locale)
+  })
 }
 
 async function convertCase(caseFunction: Function, locale: string) {
-  var nodes = getSelectedTextNodes();
+  var nodes = getSelectedTextNodes()
   if (nodes.length == 0) {
-    figma.closePlugin(`⚠️   Please select text layers first.`);
+    figma.closePlugin(`⚠️   Please select text nodes first.`)
   }
-  var pending = nodes.length;
+  var pending = nodes.length
   await Promise.all(
     nodes.map((textNode: TextNode) => {
       figma.loadFontAsync(textNode.fontName as FontName).then(() => {
-        textNode.characters = caseFunction(textNode.characters, locale);
-        pending--;
+        textNode.characters = caseFunction(textNode.characters, locale)
+        pending--
         if (pending == 0) {
-          figma.closePlugin(`✅   ${nodes.length} text nodes converted.`);
+          figma.closePlugin(`✅   ${nodes.length} text nodes converted.`)
         }
-      });
+      })
     })
-  );
+  )
 }
 
 function getLanguages() {
-  var languages = [];
+  var languages = []
   for (let lang in langmap) {
     if (lang.length <= 3) {
       languages.push({
         data: lang,
-        name: langmap[lang]["nativeName"],
-      });
+        name: langmap[lang]['nativeName'],
+      })
     }
   }
-  return languages.sort((a, b) => (a.name > b.name ? 1 : -1));
+  return languages.sort((a, b) => (a.name > b.name ? 1 : -1))
 }
 
 function getSelectedTextNodes(): Array<TextNode> {
-  const nodes = figma.currentPage.selection;
-  const result: Array<TextNode> = [];
+  const nodes = figma.currentPage.selection
+  const result: Array<TextNode> = []
   for (const node of nodes) {
     traverseNode(node, function (childNode: SceneNode) {
-      if (childNode.type !== "TEXT") {
-        return;
+      if (childNode.type !== 'TEXT') {
+        return
       }
-      result.push(childNode);
-    });
+      result.push(childNode)
+    })
   }
-  return result;
+  return result
 }
 
 function sentenceCaseFunction(text: string, locale: string) {
   return text
     .toLocaleLowerCase(locale)
-    .replace(/^\s*\w|[.?!…(...)]\s*./gu, (s) => s.toLocaleUpperCase(locale));
+    .replace(/^\s*\w|[.?!…(...)]\s*./gu, s => s.toLocaleUpperCase(locale))
 }
 
 function titleCaseFunction(text: string, locale: string) {
   return text.replace(/([^\s:-])([^\s:-]*)/gu, ($0, $1, $2) => {
-    return $1.toLocaleUpperCase(locale) + $2.toLocaleLowerCase(locale);
-  });
+    return $1.toLocaleUpperCase(locale) + $2.toLocaleLowerCase(locale)
+  })
 }
 
 function upperCaseFunction(text: string, locale: string) {
-  return text.toLocaleUpperCase(locale);
+  return text.toLocaleUpperCase(locale)
 }
 
 function lowerCaseFunction(text: string, locale: string) {
-  return text.toLocaleLowerCase(locale);
+  return text.toLocaleLowerCase(locale)
 }
